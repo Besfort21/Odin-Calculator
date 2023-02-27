a6016119a4cad36b0b211d9a502f4ccd0a6ad016
Besfort21 <besfort-avdyli1@hotmail.com>
2023-02-27 17:25:12 +0100

    js calculator fucntions and first html and css layout

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mult(a,b){
+    return a * b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+function operate(operator,number1,number2){
+    return operator(number1,number2);
+}
